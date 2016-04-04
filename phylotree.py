751f14ebbd8317cb7c1ba285d5147506db643d55
Sam Vohr <svohr@soe.ucsc.edu>
2016-04-04 14:09:47 -0700

    Added stub file for parsing markers and haplogroups from phylotree.

---
+++ phylotree.py
@@ -0,0 +1,10 @@
+"""
+
+This file contains functions for reading in phylotree variants and haplogroups
+and building a matrix to represent these markers.
+
+Mon Apr  4 12:00:54 PDT 2016
+
+"""
+
+import numpy
