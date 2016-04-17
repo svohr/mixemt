edc382d0650d49e2363670bfa5295e81d1a13344
Sam Vohr <svohr@soe.ucsc.edu>
2016-04-17 14:19:42 -0700

    Added function in 'assembly' to report findings.
    Started functions for writing out contributor specific SAM/BAM files.

--- assemble.py
+++ assemble.py
@@ -16,6 +16,7 @@ Wed Apr 13 10:57:39 PDT 2016
 
 import numpy
 import operator
+import pysam
 
 
 def get_contributors(haplogroups, props, read_hap_mat, min_prob, min_reads):
@@ -63,16 +64,44 @@ def assign_reads(contribs, haps, reads, read_hap_mat, min_prob):
         contrib_reads[hap] = set(numpy.nonzero(read_hap_mat[:, hap_col]
                                                 >= min_prob)[0])
         unassigned -= contrib_reads[hap]
-    return contrib_reads, unassigned
+    contrib_reads['unassigned'] = unassigned
+    return contrib_reads
 
 
-#def build_hap_output_table(samfile
+def report_contributors(out, contribs, contrib_reads, wts, header=True):
+    """
+    Prints a table that summarizes the contributors, their proportions and 
+    number of reads assigned to each.
+    """
+    for hap_id, haplogroup, prop in contribs:
+        total_reads = 0
+        for read_index in contrib_reads[hap_id]:
+            total_reads += wts[read_index]
+        out.write('%s\t%s\t%.4f\t%d\n' % (hap_id, haplogroup,
+                                          prop, total_reads))
+    return
 
-def write_haplotype_sams(orig_samfile):
+
+def get_contrib_read_ids(indexes, reads, reads_sigs):
+    """
+    Takes a set of indexes from assign_reads and the list of read signatures
+    plus the dictionary mapping signatures to aligned read IDs and returns
+    the set of corresponding aligned read IDs (SAM/BAM query IDs).
+    """
+    hap_read_ids = set()
+    for read_idx in indexes:
+        for read_id in read_sigs[reads]:
+            hap_read_ids.add(read_id)
+    return hap_read_ids
+
+
+def write_haplotype_sam(samfile, hap_sam, hap_reads):
     """
     Takes in a SAM/BAM AlignmentFile and read haplotype assignments and
     makes new SAM/BAM files containing the subset of reads assigned to each
     haplogroup.
     """
-
+    for aln in samfile:
+        if aln in hap_reads:
+            hap_sam.write(aln)
     return
