56e76bd16f6877d61ec1ecf73b527624d6e489bc
Sam Vohr <svohr@soe.ucsc.edu>
2016-05-11 10:51:16 -0700

    Added new file "observe.py" for building tables of base counts at ref pos.

---
+++ observe.py
@@ -0,0 +1,21 @@
+"""
+observe.py
+
+This module contains functions for building a table of counts of all bases
+observed at all reference positions. This table will be used to associate novel
+variants observed in the sample with the haplogroups and variants known from
+the tree. We can also use this table to confirm that variants from the tree
+are observed within this sample.
+
+Sam Vohr (svohr@soe.ucsc.edu)
+Wed May 11 10:47:05 PDT 2016
+
+"""
+
+def build_obs_table(samfile):
+    """
+    Returns a table of counts for every base observed at every reference
+    position in the fragments contained in the provided samfile.
+    """
+    return
+
