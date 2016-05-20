973818c3b11269b72ab22d3651d5897c983dafcd
Sam Vohr <svohr@soe.ucsc.edu>
2016-05-20 14:13:52 -0700

    New file for report functions + outputing statistics for plotting.

---
+++ stats.py
@@ -0,0 +1,13 @@
+"""
+stats.py
+
+This file contains functions for reporting results from EM and assembly steps
+and raw numbers for visualization with another tool.
+
+Sam Vohr (svohr@soe.ucsc.edu)
+Fri May 20 14:04:02 PDT 2016
+"""
+
+
+import sys
+import numpy
