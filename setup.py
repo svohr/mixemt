0cedff972c88602d4ba1bbeb04e536713ce0e305
svohr <shvohr@gmail.com>
2017-03-01 15:54:00 -0800

    Added basic info to setup.py
    Still untested.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from distutils import setup
 
-setup()
+setup(
+    name='mixemt',
+    version='0.1',
+    description='',
+    url='http://github.com/svohr/mixemt',
+    author='Samuel H. Vohr',
+    author_email='svohr@soe.ucsc.edu',
+    license='MIT',
+    packages=['mixemt'],
+    scripts=['bin/mixemt']
+    )
