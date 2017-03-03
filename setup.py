46c5cfc84154d804e21adcd967951d9340689c69
svohr <shvohr@gmail.com>
2017-03-03 09:47:10 -0800

    Added entries for additional files to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     author_email='svohr@soe.ucsc.edu',
     license='MIT',
     packages=['mixemt'],
+    package_data={'mixemt': ['ref/rCRS.mtDNA.fa', 'ref/RSRS.mtDNA.fa',
+                             'phylotree/mtDNA_tree_Build_16.csv',
+                             'phylotree/mtDNA_tree_Build_17.csv',
+                             'ref/README.txt', 'phylotree/README.md']},
+    data_files=[('plot', ['plot/plot_hap_coverage.R',
+                          'plot/plot_mix_coverage.R'])],
     scripts=['bin/mixemt']
     )
